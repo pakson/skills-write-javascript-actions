42c73ac5b529165eefa1856871736d1cab41fa75
Pawel Kowalczyk <paksonpl@gmail.com>
2026-05-04 23:52:38 +0000

    Add ncc build script and bundled dist/index.js (rest of changes)

--- src/joke.js
+++ src/joke.js
@@ -2,7 +2,7 @@ import request from "request-promise";
 
 const options = {
     method: "GET",
-    uri: "https://icanhazdadjoke.com",
+    uri: "https://icanhazdadjoke.com/",
     headers: {
         Accept: "application/json",
         "User-Agent": "Writing JavaScript action GitHub Skills excercise"
